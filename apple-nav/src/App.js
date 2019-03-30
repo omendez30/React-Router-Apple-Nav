bd2e65a847328cef3bc08af9149d01808aeaf05d
omendez30 <omendez9@outlook.com>
2019-03-30 13:53:12 -0400

    added dummy data and CDM

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Nav from "../../Nav";
+import DummyData from "./dummyData";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ data: DummyData });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
       </div>
     );
   }
